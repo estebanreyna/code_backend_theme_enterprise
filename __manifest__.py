4d05469f8696e82501783b2df48cea28b2b1d265
estebanreyna <estebanreynapro@gmail.com>
2021-10-04 14:02:36 -0500

    .

--- __manifest__.py
+++ __manifest__.py
@@ -37,6 +37,8 @@
     ],
     "qweb": [
         'static/src/xml/styles.xml',
+        'static/src/xml/top_bar.xml',
+        'static/src/xml/sidebar.xml',
     ],
     'images': [
         'static/description/banner.png',
